f261971ab20a02c1f3e5d98bb02c496bf6f6573a
Nabi <nabi.usonov@gmail.com>
2024-10-05 19:46:43 +0500

    Added some more tests and removed mount volume because entrypoint wont run with it

--- cats_website/cats_restapi/tests/tests.py
+++ cats_website/cats_restapi/tests/tests.py
@@ -43,13 +43,6 @@ def test_user_create_cat(api_client, get_jwt_access_token):
     assert response.data['description'] == 'test description'
     assert response.data['owner'] == 'test_user'
 
-def test_user_update_cat(api_client, get_jwt_access_token_for_username):
-    access = get_jwt_access_token_for_username(username='user1')
-    api_client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(access))
-    response = api_client.patch('/cats/1/', data={'name': 'updated cat name'})
-    assert response.status_code == 200
-    assert response.data['name'] == 'updated cat name'
-
 def test_user_get_cats(api_client, get_jwt_access_token):
     api_client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(get_jwt_access_token))
     response = api_client.get('/cats/')
@@ -72,15 +65,33 @@ def test_user_update_cat_wrong_user(api_client, get_jwt_access_token):
     response = api_client.patch('/cats/1/', data={'name': 'testcat', 'breed': 'Brittish', 'age': 2, 'color': 'red', 'description': 'test description'})
     assert response.status_code == 403
 
+def test_user_update_cat(api_client, get_jwt_access_token_for_username):
+    access = get_jwt_access_token_for_username(username='user1')
+    api_client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(access))
+    response = api_client.patch('/cats/1/', data={'name': 'updated cat name'})
+    assert response.status_code == 200
+    assert response.data['name'] == 'updated cat name'
+
 def test_user_get_cat(api_client, get_jwt_access_token):
-    pass
+    api_client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(get_jwt_access_token))
+    response = api_client.get('/cats/1/')
+    assert response.status_code == 200
 
 def test_user_rate_cat(api_client, get_jwt_access_token):
-    pass
+    api_client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(get_jwt_access_token))
+    response = api_client.post('/cats/1/ratings/', data={'rating': 5})
+    assert response.status_code == 201
 
 def test_user_rate_the_same_cat_twice(api_client, get_jwt_access_token):
-    pass
+    api_client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(get_jwt_access_token))
+    response = api_client.post('/cats/1/ratings/', data={'rating': 5})
+    assert response.status_code == 201
+
+    response = api_client.post('/cats/1/ratings/', data={'rating': 5})
+    assert response.status_code == 400
 
 def test_user_get_ratings(api_client, get_jwt_access_token):
-    pass
+    api_client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(get_jwt_access_token))
+    response = api_client.get('/cats/1/ratings/')
+    assert response.status_code == 200
 
